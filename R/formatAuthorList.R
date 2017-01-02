d38764288eb120ff10f13317bb597e58a588ae32
Richard Vidgen <richard@vidgen.com>
2017-01-02 17:33:51 +0000

    Update formatAuthorList.R

--- R/formatAuthorList.R
+++ R/formatAuthorList.R
@@ -1,26 +1,27 @@
 # extract and separate authors and trim white-space then remove spaces in names
+# remove leading comma from Jr
 
 formatAuthorList <- function(authorVector){
-auths = strsplit(authorVector, ".,", fixed=T)
-auths = lapply(auths, str_trim)
 
-fix_spaces <- function(x) {
-  gsub(" ", "", x, fixed = TRUE)
+  fix_spaces <- function(x) {
+    gsub(" ", "", x, fixed = TRUE)
+  }
+  
+  fix_periods <- function(x) {
+    gsub(".", "", x, fixed = TRUE)
+  }
+  
+  fix_commas <- function(x) {
+    gsub(",", "", x, fixed = TRUE)
+  }
+
+  auths = gsub(", Jr", " Jr", authorVector, fixed = TRUE)
+  auths = strsplit(auths, ".,", fixed=T)
+  auths = lapply(auths, str_trim)
+  
+  auths = lapply(auths, fix_spaces)
+  auths = lapply(auths, fix_periods)
+  auths = lapply(auths, fix_commas)
+
+  return(auths)
 }
-
-fix_periods <- function(x) {
-  gsub(".", "", x, fixed = TRUE)
-}
-
-fix_commas <- function(x) {
-  gsub(",", "", x, fixed = TRUE)
-}
-
-auths = lapply(auths, fix_spaces)
-auths = lapply(auths, fix_periods)
-auths = lapply(auths, fix_commas)
-
-return(auths)
-}
-
-
